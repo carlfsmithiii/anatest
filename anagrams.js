41c066740d0bec9131ac6624b25c013ac16462be
Carl Smith <carlfsmithiii@gmail.com>
2018-12-05 18:26:48 -0500

    added anagrams

---
+++ anagrams.js
@@ -0,0 +1,18 @@
+const { words } = require('./words');
+
+function getAnagramsOf(input) {
+    const result = [];
+    const key = alphabetize(input);
+    for (let word of words) {
+        if (alphabetize(word) === key) {
+            result.push(word);
+        } 
+    }
+    return result;
+}
+
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+}
+
+module.exports = { getAnagramsOf };
